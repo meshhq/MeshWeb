634648921773b1e0c11d6149d6113d38fa8f724e
Taylor Halliday <taylor.halliday@gmail.com>
2016-01-26 14:44:37 -0800

    Updating site

--- public/app/actions/users.jsx
+++
@@ -1,6 +0,0 @@
-
-import { createAction } from 'redux-actions'
-
-export const addUser = createAction('add user')
-export const deleteUser = createAction('delete user')
-export const editUser = createAction('edit user')
