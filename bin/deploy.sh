8b266837ff1b42990743c4c412c414d677310e73
Taylor Halliday <taylor.halliday@gmail.com>
2016-02-10 07:42:31 -0800

    \n🌎 🌎 Deploying Mesh to GHPages @Wed Feb 10 07:42:31 PST 2016 🌎 🌎

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,10 @@
-echo -e "\n  🌎🌎\033[0;32mDeploying updates to GitHubPages...\033[0m 🌎🌎"
+echo -e "\n 🌎 🌎\033[0;32mDeploying updates to GitHubPages...\033[0m 🌎 🌎"
 
 # Add changes to git.
 git add -A
 
 # Commit changes.
-msg="\n🌎🌎  Deploying Mesh to GHPages @`date` 🌎🌎"
+msg="\n🌎 🌎 Deploying Mesh to GHPages @`date` 🌎 🌎"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
