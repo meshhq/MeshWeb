71d6256fa2d98d1de05381089f74bec681c83429
Kevin Coleman <Kevin@Kevins-MacBook-Air.local>
2016-04-22 16:14:42 -0700

    Organizing logic and adding place holders in the table components

--- app/components/Lists/index.jsx
+++ app/components/Lists/index.jsx
@@ -8,14 +8,95 @@ import _ from 'underscore'
 class Lists extends Component {
   constructor(props, context) {
     super(props, context)
+
+    // TODO: Fetch providers from server. We should have an endpoint that just
+    // tells the app which providers are currently active for each customer.
     const providerTypesPresentInLists = _.pluck(this.props.lists, 'origin_provider')
     const uniqProviderTypesPresentInLists = _.uniq(providerTypesPresentInLists)
-    const filteredProviders = _.filter(this.props.providers, (provider) => { 
+    const filteredProviders = _.filter(this.props.providers, (provider) => {
       return _.indexOf(uniqProviderTypesPresentInLists, provider.type) != -1
     })
     filteredProviders.push(this._meshProvider())
-    this.state = { filteredProviders: filteredProviders, selectedProvider: _.last(filteredProviders) }
+
+    // Setup Our State.
+    this.state = {
+      filteredProviders: filteredProviders,
+      selectedProvider: _.last(filteredProviders)
+    }
   }
+
+  //****************************************************************************
+  // Action Bar Handlers
+  //****************************************************************************
+
+  _handleActionClick(idx) {
+    switch(idx) {
+    case 0:
+        // Select All
+        break;
+    case 1:
+        // Show form
+        break;
+    case 2:
+
+        break;
+    case 3:
+        // Merge
+        break;
+    case 4:
+        // More
+        break;
+      }
+  }
+
+  /**
+   * _handleNewList handles a click to the `New` action bar button.
+   */
+  _handleNewList() {
+    /*
+      1. Present form for List data entry.
+      2. Optimistically add list model to data source.
+      2. Add the list via API.
+     */
+  }
+
+  /**
+   * _handlePublishList handles a click to the `Publish` action bar button.
+   */
+  _handlePublishlist() {
+    /*
+      1. Present integration view with options to select one or multiple integtrations.
+      2. Publish the list to the selected integrations.
+     */
+  }
+
+  /**
+   * _handleDeleteList handles a click to the `Delete` action bar button.
+   */
+  _handleDeleteList() {
+    /*
+      1. Present deletion confirmation box.
+      2. Optimistically delete list from data source.
+      2. Delete the list via Mesh API.
+     */
+  }
+
+  //****************************************************************************
+  // List Detail
+  //****************************************************************************
+
+  /**
+   * _handleShowListDetail handles a click on the actual list in the table.
+   */
+  _handleShowListDetail() {
+    /*
+      1. Present the list detail view.
+     */
+  }
+
+  //****************************************************************************
+  // Provider Filtering Handlers
+  //****************************************************************************
 
   _listsFilteredByCurrentIntegration() {
     const lists = this.props.lists;
@@ -38,10 +119,10 @@ class Lists extends Component {
 
   render() {
     // MAKE PANELS
-    const providersForDropdown = _.filter(this.state.filteredProviders, (provider) => { 
+    const providersForDropdown = _.filter(this.state.filteredProviders, (provider) => {
       return provider.type != this.state.selectedProvider.type
     })
-    
+
     // Building the drop down top left sort
     let providerSortList = []
     for (let i = 0; i < providersForDropdown.length; i++) {
@@ -51,16 +132,16 @@ class Lists extends Component {
         <li>
           <a href="#"
             key={provider.name}
-            onClick={providerClicked} 
+            onClick={providerClicked}
           >
           {provider.name}
           </a>
         </li>
-      )      
+      )
     }
 
     // Building the drop down top left sort
-    const filteredLists = _.filter(this.props.lists, (list) => { 
+    const filteredLists = _.filter(this.props.lists, (list) => {
       const hasName = list.hasOwnProperty('name')
       const isCurrentProvider = list.origin_provider == this.state.selectedProvider.type
       return hasName && isCurrentProvider
@@ -71,7 +152,7 @@ class Lists extends Component {
 
 
       return (
-        <div className="list row" 
+        <div className="list row"
           key={list.id}
         >
           <div className="col-md-4 name-column">
@@ -83,17 +164,17 @@ class Lists extends Component {
           <div className="col-md-4 action-column">
             <div className="action-selector">
               <div className="btn-group">
-                <button 
-                  className="btn" 
-                  type="button" 
+                <button
+                  className="btn"
+                  type="button"
                 >
                 {'Send To Provider'}
                 </button>
-                <button aria-expanded="false" 
-                  aria-haspopup="true" 
-                  className="btn dropdown-toggle" 
-                  data-toggle="dropdown" 
-                  type="button" 
+                <button aria-expanded="false"
+                  aria-haspopup="true"
+                  className="btn dropdown-toggle"
+                  data-toggle="dropdown"
+                  type="button"
                 >
                   <span className="caret"></span>
                   <span className="sr-only">{'Toggle Dropdown'}</span>
@@ -102,7 +183,7 @@ class Lists extends Component {
                   <li><a href="#">{'Action'}</a></li>
                   <li><a href="#">{'Another action'}</a></li>
                   <li><a href="#">{'Something else here'}</a></li>
-                  <li className="divider" 
+                  <li className="divider"
                     role="separator"
                   >
                   </li>
@@ -117,7 +198,7 @@ class Lists extends Component {
 
     // Layout the providers in a row
     return (
-      <div className="row list-wrapper" 
+      <div className="row list-wrapper"
         key="list"
       >
         <div className="header row">
@@ -129,15 +210,15 @@ class Lists extends Component {
         <div className="col-md-12 integration-selector">
           <div className="company-selector">
             <div className="btn-group integration-selector">
-              <button 
-                className="btn integration-selector" 
+              <button
+                className="btn integration-selector"
                 type="button"
               >{this.state.selectedProvider.name}</button>
-              <button 
-                aria-expanded="false" 
+              <button
+                aria-expanded="false"
                 aria-haspopup="true"
-                className="btn integration-selector dropdown-toggle" 
-                data-toggle="dropdown" 
+                className="btn integration-selector dropdown-toggle"
+                data-toggle="dropdown"
                 type="button"
               >
                 <span className="caret"></span>
@@ -167,7 +248,7 @@ class Lists extends Component {
               </div>
             </div>
           </div>
-          
+
           <div className="content-rows row">
             {panels}
           </div>
