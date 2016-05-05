8321704084e46b43edcefaa515abb9cb407e2293
Taylor Halliday <taylor.halliday@gmail.com>
2016-05-05 10:13:35 -0700

    Deploying Mesh to AWS PRODUCTION @Thu May  5 10:13:35 PDT 2016  🌎

--- lib/deploy_aws_production.sh
+++ lib/deploy_aws_production.sh
@@ -1,5 +1,24 @@
+echo -e "\n 🌎  \033[0;32mDeploying updates to AWS PRODUCTION...\033[0m"
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Deploying Mesh to AWS PRODUCTION @`date`  🌎"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
 timestamp() {
   date +"%s"
 }
 
-aws s3 sync ./public/ s3://meshweb/`timestamp`
+# Clear the bucket
+aws s3 rm s3://bucket-name --recursive
+
+# Upload new site
+aws s3 sync ./public/ s3://meshweb
