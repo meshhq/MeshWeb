52d8dec7eab19495d5beaa0fa8a031783fbf064c
Taylor Halliday <taylor.halliday@gmail.com>
2016-06-22 12:06:47 -0700

    Deploying Mesh to AWS Development @Wed Jun 22 12:06:47 PDT 2016  🌎

---
+++ lib/deploy_aws_demo.sh
@@ -0,0 +1,22 @@
+echo -e "\n \033[0;32mDeploying updates to AWS Development...\033[0m 🌎"
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Deploying Mesh to AWS Development @`date`  🌎"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+branch_name=$(git name-rev --name-only HEAD)
+git push origin HEAD
+
+timestamp() {
+  date +"%s"
+}
+
+# Upload new site
+aws s3 sync ./public/ s3://dev.app.meshdata.io --region us-west-2
